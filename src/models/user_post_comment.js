926c3fd3ab6541d96d2e234a905e77704ec809f1
NihadMouden <nihadmouden@gmail.com>
2023-08-08 17:01:50 +0200

    feat: Implemented userPostComment & userPostLike models

---
+++ src/models/user_post_comment.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+const UserPostCommentSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+  },
+  postId: {
+    type: String,
+  },
+  comment: {
+    type: String,
+  },
+  replyTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'UserPostComment',
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+})
+const UserPostComment = mongoose.model('UserPostComment', UserPostCommentSchema)
+
+export default UserPostComment
